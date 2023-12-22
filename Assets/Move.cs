726bd12adf6c353ddefb2ad4bf89338b05ee81b1
kylai02 <dennislai120@gmail.com>
2023-12-22 16:28:57 +0800

    add forward

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,20 +6,30 @@ using Valve.VR;
 
 
 public class Move : MonoBehaviour {
-  private SteamVR_Action_Boolean forward = SteamVR_Input.GetBooleanAction("Forward");
   public SteamVR_Input_Sources curIndex;
-  public int test;
+  public float speed;
+
+  public Transform camera;
   
+  public bool forwarding;
+  public int ctr;
+  
+  private SteamVR_Action_Boolean forward = SteamVR_Input.GetBooleanAction("Forward");
+
   // Start is called before the first frame update
   void Start() {
-    test = 0;
   }
 
   // Update is called once per frame
   void Update() {
-    if (forward.GetStateDown(curIndex)) {
-      Debug.Log("forward");
-      test++;
+    forwarding = forward.GetState(curIndex);
+    
+    if (forwarding) {
+      transform.position = new Vector3(
+        transform.position.x + camera.forward.x * speed,
+        transform.position.y,
+        transform.position.z + camera.forward.z * speed
+      );
     }
   }
 }
