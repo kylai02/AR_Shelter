edf55b80ecc96169a3d330f47371ed9a8a08423e
kylai02 <dennislai120@gmail.com>
2024-02-02 21:32:48 +0800

    feat: elevator button interactable

--- Assets/AR_Shelter Scripts/SecretDoorButton.cs
+++ Assets/AR_Shelter Scripts/SecretDoorButton.cs
@@ -2,15 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
+using DG.Tweening;
 
 
 public class SecretDoorButton : MonoBehaviour {
+  [HeaderAttribute("References")]
+  [SpaceAttribute(10)]
+  
+  public GameObject secretDoorObj;
+
+  private bool _isOpen = false;
+
   public void OnButtonDown(Hand fromHand) {
-    Debug.Log("Down");
+    // Trigger Haptic on controller
     fromHand.TriggerHapticPulse(1000);
+    
+    if (!_isOpen) {
+
+      // Open the secret door
+      secretDoorObj.transform.DORotate(
+        endValue: new Vector3(0, 80, 0), 
+        duration: 2, 
+        mode: RotateMode.WorldAxisAdd
+      );
+
+      // Close the collider of the door
+      secretDoorObj.GetComponent<Collider>().enabled = false;
+
+      _isOpen = true;
+    }
   }
 
-  public void OnButtonUp(Hand fromHand) {
-    Debug.Log("Up");
-  }
+  public void OnButtonUp(Hand fromHand) {}
 }
