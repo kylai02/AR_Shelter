f5c32665f23bb191634ecc871faca2602f05596d
kylai02 <dennislai120@gmail.com>
2024-02-27 17:38:17 +0800

    add the interactable scripts' comments

--- Assets/AR_Shelter/Scripts/Interactable/SecretDoorButton.cs
+++ Assets/AR_Shelter/Scripts/Interactable/SecretDoorButton.cs
@@ -7,27 +7,26 @@ using DG.Tweening;
 
 public class SecretDoorButton : MonoBehaviour {
   [HeaderAttribute("References")]
-  [SpaceAttribute(10)]
-  
-  public GameObject secretDoorObj;
+  [SpaceAttribute(3)]
+  [SerializeField] private GameObject _secretDoorObj;
 
   private bool _isOpen = false;
 
   public void OnButtonDown(Hand fromHand) {
-    // Trigger Haptic on controller
+    // Trigger haptic on controller
     fromHand.TriggerHapticPulse(1000);
     
     if (!_isOpen) {
 
       // Open the secret door
-      secretDoorObj.transform.DORotate(
+      _secretDoorObj.transform.DORotate(
         endValue: new Vector3(0, 80, 0), 
         duration: 2, 
         mode: RotateMode.WorldAxisAdd
       );
 
       // Close the collider of the door
-      secretDoorObj.GetComponent<Collider>().enabled = false;
+      _secretDoorObj.GetComponent<Collider>().enabled = false;
 
       _isOpen = true;
     }
