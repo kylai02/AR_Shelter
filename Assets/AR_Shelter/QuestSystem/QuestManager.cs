2f958e26418b2e9b9e14071304a1febb0ae7ecb0
kylai02 <dennislai120@gmail.com>
2024-04-21 15:38:17 +0800

    update QuestSystem, complete 1-1

--- Assets/AR_Shelter/QuestSystem/QuestManager.cs
+++ Assets/AR_Shelter/QuestSystem/QuestManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,46 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour {
   public static QuestManager instance;
-  public string questStep;
+  public static event Action<string> QuestStartEvent;
+
+  [SerializeField] private List<string> questQueue = new();
+  [SerializeField] private float initQuestTimer = 5f;
+  public string questStep = "0";
+  private bool hasBegin = false;
 
   void Start() {
-    instance = this;
-    questStep = "0";
+    if (instance == null) {
+      instance = this;
+    } else {
+      Destroy(gameObject);
+    }
+
+    DontDestroyOnLoad(gameObject);
+  }
+
+  void Update() {
+    if (!hasBegin) {
+      initQuestTimer -= Time.deltaTime;
+      
+      if (initQuestTimer <= 0) {
+        StartNextQuest();
+        hasBegin = true;
+      }
+    }
+  }
+
+  public void SetQuestStep(string questId) {
+    questStep = questId;
+  }
+
+  public void StartNextQuest() {
+    try {
+      string questToStart = questQueue[0];
+      questQueue.RemoveAt(0);
+      SetQuestStep(questToStart);
+      QuestStartEvent?.Invoke(questToStart);
+    } catch {
+      return;
+    }
   }
 }
---
+++ Assets/AR_Shelter/QuestSystem/Quests/Quest_1_1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Quest_1_1 : Quest {
+  [HeaderAttribute("References")]
+  [SpaceAttribute(3)]
+  [SerializeField] private GameObject door;
+  [SerializeField] private GameObject doorTargetEffect;
+
+  protected override void StartQuest() {
+    StartDialogues();
+  }
+
+
+  public void StartWaitUntilDoorOpen() {
+    StartCoroutine(WaitUntilDoorOpen());
+  }
+
+  IEnumerator WaitUntilDoorOpen() {
+    OpenDoor openDoor = door.GetComponent<OpenDoor>();
+    yield return new WaitUntil(() => openDoor.isOpen);
+
+    doorTargetEffect.SetActive(false);
+
+    NextQuest();
+  }
+}
