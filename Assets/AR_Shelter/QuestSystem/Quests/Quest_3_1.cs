e5b551f6be9e3b9b78439ec5fd6d57043cbb580d
kylai02 <dennislai120@gmail.com>
2024-05-01 20:30:41 +0800

    complete whole story

--- Assets/AR_Shelter/QuestSystem/Quests/Quest_3_1.cs
+++ Assets/AR_Shelter/QuestSystem/Quests/Quest_3_1.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 
 public class Quest_3_1 : Quest {
   [HeaderAttribute("References")]
   [SpaceAttribute(3)]
   [SerializeField] private GameObject door;
+  [SerializeField] private GameObject secretDoor;
+  [SerializeField] private GameObject animationObj;
+  [SerializeField] private GameObject glasses;
 
   protected override void StartQuest() {
     StartCoroutine(QuestRoutine());
@@ -18,5 +21,16 @@ public class Quest_3_1 : Quest {
     yield return new WaitForSeconds(2f);
 
     StartDialogues();
+
+    openDoor = secretDoor.GetComponent<OpenDoor>();
+    yield return new WaitUntil(() => openDoor.isOpen);
+    yield return new WaitForSeconds(2f);
+
+    animationObj.SetActive(true);
+    yield return new WaitForSeconds(10f);
+    animationObj.SetActive(false);
+
+    yield return new WaitForSeconds(1.5f);
+    glasses.transform.DOLocalMoveY(0.4f, 4).SetEase(Ease.OutCubic);
   }
 }
